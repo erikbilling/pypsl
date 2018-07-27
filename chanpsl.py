7ee37edc2814eb962558fe4f4140372dd008c630
Erik A. Billing <erik.billing@his.se>
2018-07-28 00:12:18 +0200

    First working version of chanspl, with prediction

--- chanpsl.py
+++ chanpsl.py
@@ -4,7 +4,7 @@ using a channel coded data representation.
 """
 
 import numpy as np
-from chanpy import Cos2ChannelBasis
+from chanpy import Cos2ChannelBasis, ChannelVector
 
 class ChanPsl(object):
     def __init__(self, nChannels=11, minValue=0., maxValue=1., channelBasis=None, memory=None):
@@ -14,7 +14,7 @@ class ChanPsl(object):
             self.__basis__.setParameters(nChannels, minValue, maxValue)
         
     def encode(self,s):
-        return self.__basis__.encode(s)
+        return ChannelVector(self.__basis__) if s is None else self.__basis__.encode(s)
 
     def trainSample(self,history,target):
         v = self.encode(history[-1])
@@ -24,3 +24,15 @@ class ChanPsl(object):
         else: 
             assert v.size**2 == self.memory.size
         self.memory += v.transpose()*t/1.125
+
+    def predict(self,history):
+        return self.predictSample(history[-1] if history else None)
+
+    def predictSample(self,value):
+        res = self.predictVector(self.encode(value))
+        return res.decode().ravel()[0]
+
+    def predictVector(self,v):
+        res = ChannelVector(self.__basis__)
+        res[:] = np.dot(v,self.memory + np.random.rand(*self.memory.shape)*10)
+        return res
