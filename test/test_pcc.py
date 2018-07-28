56312cdb57523a533acec73b93d66fdae11147cf
Erik A. Billing <erik.billing@his.se>
2018-07-28 23:03:21 +0200

    Updated unit test

--- test/test_pcc.py
+++ test/test_pcc.py
@@ -1,27 +1,27 @@
 
 import numpy as np
 import unittest
-from inputdata import singen
+from test.inputdata import singen
 from pcc import Pcc
 
 class TestChanPsl(unittest.TestCase):
 
     def setUp(self):
-        self.pcc = Pcc()
+        self.pcc = Pcc(25,-1,1)
 
     def test_sinus_prediction(self):
         history = []
         N = 100
-        for v in singen(stop=N):
+        data = [v for v in singen(stop=N)]
+        for v in data:
             if history:
                 self.pcc.trainSample(history,target=v)
             history.append(v)
         self.assertEqual(np.round(self.pcc.memory.sum()),N*20)
-
-        data = [v for v in singen(stop=N)]
         result = [self.pcc.predictSample(data[i-1] if i > 0 else None) for i,v in enumerate(data)]
         mse = np.array([(p-v)*(p-v) for p,v in zip(result,data)]).mean()
-        self.assertLess(mse,0.000)       
+        self.assertLess(mse,0.01)       
+        
 
 if __name__ == '__main__':
     unittest.main()
