426ed375debeb68cc20dc85a104730d047adee83
Erik A. Billing <erik.billing@his.se>
2018-08-24 13:02:08 +0200

    Fix of relative imports for pcc and pypsl

--- pcc/__init__.py
+++ pcc/__init__.py
@@ -2,4 +2,4 @@
 PyPsl is a library for sequence learning, implementing Predictive Sequence Learning (PSL)
 """
 
-from pcc import *
+from .pcc import Pcc, UniformChannelEncoder, LogDiffEncoder, MeanBuffer, integrate
